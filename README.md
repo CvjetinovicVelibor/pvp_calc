66cd50ab35f00bac5b05c587eafd0518c095736d
Velibor <velibor.cvj@gmail.com>
2020-12-15 16:40:40 +0100

    final version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pvp_calc
+
+## Simple website in plain html, css and javascript. 
+## Start: Hier we can enter the coordinates of his photovoltaic (PV) system.
+## System Details: Here we can provide additional data about the system and the location is displayed on a map.
+## Results: Here the resulting report is displayed. This data is static and just for the demonstration of variouse js functions. We have a data table with a search field where we can filter the data based on the week. Next we have two static images(graph and map) and two icons for (download and print)
+## Users: Here we fetch user data from https://reqres.in/api/users and display it in a table. By clicking on the user he will be placed in the selected users basket. By clicking on the user in the basket he can be removed again. 
+## Cog icon(top right): Here we see a formular whit some validations. After all data is entered correctlly the user gets a confirmation message and his name is displayed in the to right corner.
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,6 @@
 
 
 
-
 class SystemData {
   constructor(latitude, longitude, dc, efficiency, tilt, azimuth, rate) {
     this.latitude = latitude;
@@ -236,6 +235,28 @@ function validateform(){
   }
 }  
 
+////////////////////////////// Table /////////////////////////////////
+
+function searchWeek() {
+  var input, filter, table, tr, th, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("resultsTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop all rows and hide rows which dont match the search
+  for (i = 0; i < tr.length; i++) {
+    th = tr[i].getElementsByTagName("th")[0];
+    if (th) {
+      txtValue = th.textContent || th.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 
 ////////////////////// Page Initialization /////////////////////////
 function updateDisplayName(){ 
@@ -265,8 +286,53 @@ function downloadResults() {
 
 
 
+////////////////////////////// Chart //////////////////////////////////
+
+function initChart() {
+  var Chart = require('chart.js');
+  var ctx = document.getElementById('myChart');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+  });
+}  
+
+
 
 getData();
+initChart();
 //getData("https://reqres.in/api/users");
 //postData("https://reqres.in/api/users", testObject);
 
